a33e38fba6d162470924a565bdbae39c37aae0ee
Zhuravlev Ilya <97960428+Zhura387@users.noreply.github.com>
2022-11-04 20:39:05 +0300

    add cards

---
+++ weather/src/Pages/Home/Components/Days/Tabs.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import s from './Days.module.scss'
+
+const Tabs =()=>{
+
+    const tabs = [
+        {
+          value: 'На неделю',
+        },
+        {
+          value: 'На 10 дней',
+        },
+        {
+          value: 'На месяц',
+        },
+      ];
+
+return(
+<div className={s.tabs}>
+<div className={s.tabsWrapper}>
+    {tabs.map((item)=>
+    <div className={s.tab}>{item.value}</div>
+    )}
+</div>
+<div className={s.cancel}>отменить</div>
+</div>
+)
+}
+export default Tabs
--- weather/src/Pages/Home/Home.js
+++ weather/src/Pages/Home/Home.js
@@ -2,14 +2,16 @@ import React from 'react';
 import s from './Home.module.scss'
 import ThisDay from './Components/ThisDay/ThisDay';
 import ThisDayInfo from './Components/ThisDayInfo/ThisDayInfo';
-const Home =()=>{
-return(
-<div className={s.home}>
-    <div className={s.wrapper}>
-<ThisDay/>
-<ThisDayInfo/>
-</div>
-</div>
-)
+import Days from './Components/Days/Days';
+const Home = () => {
+    return (
+        <div className={s.home}>
+            <div className={s.wrapper}>
+                <ThisDay />
+                <ThisDayInfo />
+            </div>
+            <Days />
+        </div>
+    )
 }
 export default Home
